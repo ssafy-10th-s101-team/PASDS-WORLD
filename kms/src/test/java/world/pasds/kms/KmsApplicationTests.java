8929cecd84e6cf3974818a87d83debc981d1601a
이준범 <1996joon@naver.com>
2024-04-22 13:18:06 +0900

    [KMS] init: S10P31S101-106 <KMS boot 생성>

---
+++ kms/src/test/java/world/pasds/kms/KmsApplicationTests.java
@@ -0,0 +1,13 @@
+package world.pasds.kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

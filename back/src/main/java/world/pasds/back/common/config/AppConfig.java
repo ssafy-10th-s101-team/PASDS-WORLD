5d3f6c74c53a56e94496068efe15a34a10409eef
이하은 <hahi1108@gmail.com>
2024-04-24 13:18:09 +0900

    [BE] feat: <AesUtil 빈 등록>

--- back/src/main/java/world/pasds/back/common/config/AppConfig.java
+++ back/src/main/java/world/pasds/back/common/config/AppConfig.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import world.pasds.back.common.util.AesUtil;
+
 @Configuration
 public class AppConfig {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public AesUtil aesUtil() {
+		return new AesUtil();
 	}
 }

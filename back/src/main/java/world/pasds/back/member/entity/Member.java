83d88ed9f94b637c96d69a4baf9dd5b612b13ffb
이하은 <hahi1108@gmail.com>
2024-04-24 14:49:55 +0900

    [BE] rebase

--- back/src/main/java/world/pasds/back/member/entity/Member.java
+++ back/src/main/java/world/pasds/back/member/entity/Member.java
@@ -3,6 +3,7 @@ package world.pasds.back.member.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import world.pasds.back.common.BaseEntity;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -19,12 +20,15 @@ public class Member extends BaseEntity {
 
     private String password;
 
+    @Setter
     @Column(length = 32, columnDefinition = "BINARY(32)")
     private byte[] encryptedTotpKey;
 
+    @Setter
     @Column(length = 32, columnDefinition = "BINARY(32)")
     private byte[] encryptedTotpDataKey;
 
+    @Setter
     @Column(length = 32, columnDefinition = "BINARY(32)")
     private byte[] encryptedTotpIvKey;
 

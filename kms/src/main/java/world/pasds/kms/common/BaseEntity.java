caf63d0d11900d969ff18b8d9bbe09a1a6a50506
이준범 <1996joon@naver.com>
2024-04-24 09:52:15 +0900

    [KMS] feat: <BaseEntity.java가 BaseTimeEntity.java 역할까지 수행하도록 함>

--- kms/src/main/java/world/pasds/kms/common/BaseEntity.java
+++ kms/src/main/java/world/pasds/kms/common/BaseEntity.java
@@ -6,8 +6,12 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -20,4 +24,10 @@ public abstract class BaseEntity {
 
 	@LastModifiedBy
 	private String modifiedBy;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
 }

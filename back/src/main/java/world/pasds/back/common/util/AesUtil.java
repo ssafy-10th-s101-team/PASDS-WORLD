1c368646c31d581f430aa367fdca7d32b5ff268f
이하은 <hahi1108@gmail.com>
2024-04-25 17:01:36 +0900

    [BE] rebase

--- back/src/main/java/world/pasds/back/common/util/AesUtil.java
+++ back/src/main/java/world/pasds/back/common/util/AesUtil.java
@@ -15,65 +15,80 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import world.pasds.back.common.exception.BusinessException;
+import world.pasds.back.common.exception.ExceptionCode;
+
 public class AesUtil {
 
 	//키 생성
-	public byte[] keyGenerator() throws NoSuchAlgorithmException {
-		// AES 알고리즘 확인
-		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	public byte[] keyGenerator() {
+		try {
+			// AES 알고리즘 확인
+			KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
 
-		// 랜덤 시드 생성.
-		SecureRandom random = new SecureRandom();
+			// 랜덤 시드 생성.
+			SecureRandom random = new SecureRandom();
 
-		//256비트 키 생성
-		keyGenerator.init(256, random);
+			//256비트 키 생성
+			keyGenerator.init(256, random);
 
-		//키 생성
-		SecretKey secretKey = keyGenerator.generateKey();
+			//키 생성
+			SecretKey secretKey = keyGenerator.generateKey();
 
-		//byte[]로 변환후 리턴
-		return secretKey.getEncoded();
+			//byte[]로 변환후 리턴
+			return secretKey.getEncoded();
+		} catch (NoSuchAlgorithmException e) {
+			throw new BusinessException(ExceptionCode.KEY_ERROR);
+		}
 	}
 
 	//암호화
-	public byte[] encrypt(String plain, byte[] key, byte[] iv) throws
-		NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
-		IllegalBlockSizeException, BadPaddingException {
-		SecretKey secretKey = new SecretKeySpec(key, "AES");
-		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+	public byte[] encrypt(String plain, byte[] key, byte[] iv) {
+		try {
+			SecretKey secretKey = new SecretKeySpec(key, "AES");
+			IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
 
-		//암호화 Cipher 초기화
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
+			//암호화 Cipher 초기화
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
 
-		//암호화할 문자열
-		byte[] plainBytes = plain.getBytes();
+			//암호화할 문자열
+			byte[] plainBytes = plain.getBytes();
 
-		//암호화
-		byte[] cipherBytes = cipher.doFinal(plainBytes);
+			//암호화
+			return cipher.doFinal(plainBytes);
 
-		return cipherBytes;
+		} catch (NoSuchPaddingException |
+				 NoSuchAlgorithmException |
+				 InvalidAlgorithmParameterException |
+				 IllegalBlockSizeException |
+				 BadPaddingException |
+				 InvalidKeyException e) {
+			throw new BusinessException(ExceptionCode.KEY_ERROR);
+		}
 	}
 
 	//복호화
-	public String decrypt(byte[] cipherBytes, byte[] key, byte[] iv) throws
-		NoSuchPaddingException,
-		NoSuchAlgorithmException,
-		InvalidAlgorithmParameterException,
-		InvalidKeyException,
-		IllegalBlockSizeException,
-		BadPaddingException,
-		InvalidKeyException {
-		SecretKey secretKey = new SecretKeySpec(key,"AES");
+	public String decrypt(byte[] cipherBytes, byte[] key, byte[] iv) {
+		try {
+			SecretKey secretKey = new SecretKeySpec(key,"AES");
 
-		//암호화 Cipher 초기화
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			//암호화 Cipher 초기화
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
 
-		//복호화
-		byte[] plainBytes = cipher.doFinal(cipherBytes);
+			//복호화
+			byte[] plainBytes = cipher.doFinal(cipherBytes);
 
-		//String형태로 바꾼 후 리턴
-		return new String(plainBytes, StandardCharsets.UTF_8);
+			// String 형태로 바꾼 후 리턴
+			return new String(plainBytes, StandardCharsets.UTF_8);
+		} catch (NoSuchPaddingException |
+				 NoSuchAlgorithmException |
+				 InvalidAlgorithmParameterException |
+				 IllegalBlockSizeException |
+				 BadPaddingException |
+				 InvalidKeyException e) {
+			throw new BusinessException(ExceptionCode.KEY_ERROR);
+		}
 	}
 }

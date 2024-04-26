acf8ddc6e09f7cf6e2de979ee9015a37cfdd4fcd
이하은 <hahi1108@gmail.com>
2024-04-26 16:58:35 +0900

    [BE] feat: S10P31S101-108 <EmailService 작성>

--- back/src/main/java/world/pasds/back/common/service/EmailService.java
+++ back/src/main/java/world/pasds/back/common/service/EmailService.java
@@ -1,25 +1,31 @@
 package world.pasds.back.common.service;
 
 import lombok.RequiredArgsConstructor;
+
+import org.apache.logging.log4j.message.SimpleMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String sender;
+    public void sendEmail(String toEmail, String subject, String text){
+        SimpleMailMessage emailForm = createEmailForm(toEmail, subject, text);
+        mailSender.send(emailForm);
+    }
 
-    public void sendSimpleMessage(String to, String subject, String text) {
+    private SimpleMailMessage createEmailForm(String toEmail, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(sender);
-        message.setTo(to);
+        message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(text);
-        mailSender.send(message);
+        return message;
     }
 }

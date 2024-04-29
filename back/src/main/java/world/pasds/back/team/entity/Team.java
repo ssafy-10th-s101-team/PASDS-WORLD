33af03ed4916ee948ede269c3374b01cff01dee0
이준범 <1996joon@naver.com>
2024-04-29 16:55:24 +0900

    [BE] fix: <Team.java 필요한 필드에만 @Setter 적용>

--- back/src/main/java/world/pasds/back/team/entity/Team.java
+++ back/src/main/java/world/pasds/back/team/entity/Team.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -33,9 +32,11 @@ public class Team extends BaseEntity {
 
     private Integer secretCount;
 
+    @Setter
     @Column(length = 32, columnDefinition = "BINARY(32)")
     private byte[] encryptedDataKey;
 
+    @Setter
     @Column(length = 32, columnDefinition = "BINARY(32)")
     private byte[] encryptedIv;
 

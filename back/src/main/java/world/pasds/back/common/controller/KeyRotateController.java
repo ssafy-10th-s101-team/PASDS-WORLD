fe0164e26799e166159feceea40404059b87f94c
이준범 <1996joon@naver.com>
2024-04-29 15:10:57 +0900

    [BE] fix: <TeamService 의존성 주입으로 변경>

--- back/src/main/java/world/pasds/back/common/controller/KeyRotateController.java
+++ back/src/main/java/world/pasds/back/common/controller/KeyRotateController.java
@@ -1,25 +1,26 @@
 package world.pasds.back.common.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import world.pasds.back.common.service.KeyService;
-import world.pasds.back.privateData.service.PrivateDataService;
+import world.pasds.back.team.service.TeamService;
 import world.pasds.back.totp.service.TotpService;
 
 @RestController
 @RequestMapping("/app/api/key-rotate")
 @RequiredArgsConstructor
+@Slf4j
 public class KeyRotateController {
 
     private final TotpService totpService;
-    private final PrivateDataService privateDataService;
+    private final TeamService teamService;
 
     @PostMapping("/handle-masterkey-change")
     public void handleMasterKeyChange(){
-
+        log.info("masterkey-change-detected");
         totpService.refreshByMasterKey();
-        privateDataService.refreshByMasterKey();
+        teamService.refreshByMasterKey();
     }
 }

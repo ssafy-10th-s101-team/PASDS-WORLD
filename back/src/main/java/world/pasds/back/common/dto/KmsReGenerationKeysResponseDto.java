29af3695f04389d207fd24e18a048bc31c172c07
이준범 <1996joon@naver.com>
2024-05-01 15:50:31 +0900

    [BE] feat: <KeyService.java reGenerateKey() 리턴 DTO 클래스 생성>

---
+++ back/src/main/java/world/pasds/back/common/dto/KmsReGenerationKeysResponseDto.java
@@ -0,0 +1,14 @@
+package world.pasds.back.common.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class KmsReGenerationKeysResponseDto {
+    private String oldDataKey;
+    private String oldIv;
+    private String newDataKey;
+    private String newIv;
+    private String encryptedNewDataKey;
+    private String encryptedNewIv;
+}

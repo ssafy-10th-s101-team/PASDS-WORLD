a2739491bb3b8b0fae7b14bee58c03a5ba7cc32f
jinyong3512 <wlsdyd4@naver.com>
2024-05-01 17:50:12 +0900

    S10P31S101-210 <프론트 로그인 API 연결>

--- back/src/main/java/world/pasds/back/common/exception/ExceptionCode.java
+++ back/src/main/java/world/pasds/back/common/exception/ExceptionCode.java
@@ -7,7 +7,7 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "Member exists"),
     MEMBER_UNAUTHORIZED(401, "Unauthorized member control"),
 
-    EMAIL_EXISTS(409, "Email exists"),
+    EMAIL_EXISTS(409, "존재하는 이메일입니다"),
     EMAIL_NOT_FOUND(404, "Email not found"),
     EMAIL_INVALID_FORMAT(400, "Email invalid format"),
 
@@ -15,10 +15,10 @@ public enum ExceptionCode {
     EMAIL_CODE_GENERATION_ERROR(500,"Email code generation error"),
 
     PASSWORD_SAME(409, "Password is same as before"),
-    PASSWORD_INVALID_FORMAT(400, "Password invalid format"),
-    PASSWORD_CONFIRM_INVALID(400, "Password confirm invalid"),
+    PASSWORD_INVALID_FORMAT(400, "비밀번호가 유효하지 않습니다"),
+    PASSWORD_CONFIRM_INVALID(400, "비밀번호 확인이 일치하지 않습니다"),
 
-    NICKNAME_INVALID_FORMAT(400, "Nickname invalid format"),
+    NICKNAME_INVALID_FORMAT(400, "닉네임이 유효하지 않습니다"),
 
     ORGANIZATION_NOT_FOUND(404, "Organization not found"),
     ORGANIZATION_UNAUTHORIZED(401, "Unauthorized organization control"),
@@ -39,24 +39,24 @@ public enum ExceptionCode {
 
     TEAM_NAME_CONFLICT(403, "Team name already exists"),
 
-    JWT_EXPIRED(401, "Expired JWT token"),
-    JWT_UNSUPPORTED(404, "Unsupported JWT token"),
-    JWT_MALFORMED(404, "Invalid JWT token"),
-    JWT_SIGNATURE(404, "Invalid JWT signature"),
-    JWT_ARGUMENT(404, "JWT claims string is empty"),
-
     KEY_ERROR(500,"key error"),
 
     TOTP_CODE_GENERATION_ERROR(500,"Totp code generation error"),
 
-    TOTP_CODE_NOT_SAME(409, "Totp code not same"),
+    TOTP_CODE_NOT_SAME(400, "TOTP 코드를 잘못 입력했습니다\n" +
+            "입력하신 내용을 다시 확인해주세요"),
 
     NOTIFICATION_NOT_FOUND(404, "Notification not found"),
 
     ROLE_NOT_FOUND(404, "Role not found"),
     ROLE_EXISTS(409, "Role exists"),
 
-    INTERNAL_SERVER_ERROR(500,"Something wrong in server");
+    INTERNAL_SERVER_ERROR(500,"Something wrong in server"),
+
+    FIRST_LOGIN_AUTHENTICAT_FAIL(400,"이메일 또는 비밀번호를 잘못 입력했습니다\n" +
+                                         "입력하신 내용을 다시 확인해주세요"),
+    TEMPORARY_TOKEN_EXPIRED(401,"temporaryToken이 유효하지 않습니다"),
+    REFRESH_TOKEN_EXPIRED(401,"refreshToken이 유효하지 않습니다");
 
     @Getter
     private int status;

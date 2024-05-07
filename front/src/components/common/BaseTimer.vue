0b6c84d454174a08f6365ceec40e047ac3fa786b
이하은 <hahi1108@gmail.com>
2024-05-07 23:25:06 +0900

    [FE] feat: <회원 인증 타이머 추가>

---
+++ front/src/components/common/BaseTimer.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="timer" v-if="message === null"> {{ min }} : {{ sec }} </span>
+  <span class="msg" v-else> {{ message }} </span>
+</template>
+
+<script setup>
+import { useCommonStore } from '@/stores/common.js'
+import { toRefs } from 'vue'
+const commonStore = useCommonStore();
+const { min, sec, message } = toRefs(commonStore);
+
+
+</script>
+
+<style scoped>
+.timer {
+  flex-direction: row;
+  margin-left: 10px;
+  font-weight: 700;
+  color: #12279e;
+}
+.msg {
+  flex-direction: row;
+  margin-left: 10px;
+  font-weight: 700;
+  color: #12279e;
+}
+</style>

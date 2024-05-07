a9d5324c0e24c1d2a59b7f5efb1586f36e25767e
imdeokey <imdeokey@gmail.com>
2024-05-07 13:38:58 +0900

    [FE] fix: <빠진 것 수정>

--- front/src/components/common/OrganizationInvitationModal.vue
+++ front/src/components/common/OrganizationInvitationModal.vue
@@ -2,7 +2,19 @@
   <BaseModal modalId="organizationInvitationModal">
     <form class="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" action="#">
       <h3 class="text-xl font-medium text-gray-900 dark:text-white">조직 초대</h3>
-      <BaseInputTextField inputText="email" placeHolder="name@domain.com" />
+      <div>
+        <label for="email" class="block mb-2 text-sm text-gray-900 dark:text-gray-300"
+          >이메일</label
+        >
+        <input
+          type="email"
+          id="email"
+          v-model="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-samsung-blue focus:border-samsung-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@domain.com"
+          required
+        />
+      </div>
 
       <button
         type="submit"
@@ -21,7 +33,6 @@
 
 <script setup>
 import BaseModal from './BaseModal.vue'
-import BaseInputTextField from './BaseInputTextField.vue'
 </script>
 
 <style scoped></style>

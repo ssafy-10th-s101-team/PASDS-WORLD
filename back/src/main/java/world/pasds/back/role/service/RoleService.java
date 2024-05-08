27de5b708bb438e7f1f31578fe8efb01d4183876
wooseobee <dntjq0807@naver.com>
2024-05-08 14:30:43 +0900

    [BE] fix: <역할 서비스 - 역할 생성 수정>

--- back/src/main/java/world/pasds/back/role/service/RoleService.java
+++ back/src/main/java/world/pasds/back/role/service/RoleService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import world.pasds.back.authority.entity.Authority;
 import world.pasds.back.authority.entity.AuthorityName;
+import world.pasds.back.authority.repository.AuthorityRepository;
 import world.pasds.back.common.exception.BusinessException;
 import world.pasds.back.common.exception.ExceptionCode;
 import world.pasds.back.member.entity.Member;
@@ -40,6 +41,7 @@ public class RoleService {
     private final MemberRepository memberRepository;
     private final MemberRoleRepository memberRoleRepository;
     private final MemberTeamRepository memberTeamRepository;
+    private final AuthorityRepository authorityRepository;
 
     @Transactional
     public List<GetRoleResponseDto> getRole(Long teamId, Long memberId) {
@@ -90,7 +92,8 @@ public class RoleService {
         Role savedRole = roleRepository.save(createRole);
 
         List<RoleAuthority> saveRoleAuthorityList = new ArrayList<>();
-        for (Authority authority : requestDto.getAuthorities()) {
+        for (Long authorityId : requestDto.getAuthorities()) {
+            Authority authority = authorityRepository.findById(authorityId).orElseThrow(() -> new BusinessException(ExceptionCode.AUTHORITY_NOT_FOUND));
             if (AuthorityName.TEAM_DELETE == authority.getName()) {    // 팀 삭제 권한이 있는 역할 생성 불가
                 throw new BusinessException(ExceptionCode.BAD_REQUEST);
             }

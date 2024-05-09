a3f8e4af4dbfd336b17f00ec2b1bbb7393448e37
유광우 <godsun7892@naver.com>
2024-05-09 12:41:51 +0900

    [FE] fix: <메인페이지 2차 수정>

---
+++ front/src/components/dashboard/DashboardMainpage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1>메인페이지 뷰화면입니다</h1>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped></style>
---
+++ front/src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h1>메인 페이지 뷰</h1>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped></style>

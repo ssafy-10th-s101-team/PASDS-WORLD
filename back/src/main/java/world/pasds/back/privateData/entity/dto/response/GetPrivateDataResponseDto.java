92ac5ef9cfa023ed5261b309188f8789b89477d4
wooseobee <dntjq0807@naver.com>
2024-05-09 14:11:12 +0900

    [BE] fix: <비밀 서비스 - 비밀 상세 조회 수정>

--- back/src/main/java/world/pasds/back/privateData/entity/dto/response/GetPrivateDataResponseDto.java
+++ back/src/main/java/world/pasds/back/privateData/entity/dto/response/GetPrivateDataResponseDto.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import world.pasds.back.privateData.entity.DataType;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetPrivateDataResponseDto {
+    private DataType type;
+    private String title;
     private String privateData;
+    private String memo;
+    private String privateDataId;
+    private String url;
 }

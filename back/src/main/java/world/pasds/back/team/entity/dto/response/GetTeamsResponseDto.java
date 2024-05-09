6287e371b318ef41aaed4859a379faa6b7f23459
이준범 <1996joon@naver.com>
2024-05-09 14:08:01 +0900

    [BE] refactor: <GetTeamResponseDto에 빌더패턴 추가>

--- back/src/main/java/world/pasds/back/team/entity/dto/response/GetTeamsResponseDto.java
+++ back/src/main/java/world/pasds/back/team/entity/dto/response/GetTeamsResponseDto.java
@@ -1,9 +1,11 @@
 package world.pasds.back.team.entity.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class GetTeamsResponseDto {
     private Long organizationId;

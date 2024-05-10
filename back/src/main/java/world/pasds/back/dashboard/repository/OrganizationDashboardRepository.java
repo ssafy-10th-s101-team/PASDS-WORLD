2b47704233295a83984950e1d2ee67743a4134f5
유광우 <godsun7892@naver.com>
2024-05-10 16:08:34 +0900

    [BE] fix: <대시보드 동시성 에러 해결2>

--- back/src/main/java/world/pasds/back/dashboard/repository/OrganizationDashboardRepository.java
+++ back/src/main/java/world/pasds/back/dashboard/repository/OrganizationDashboardRepository.java
@@ -1,6 +1,8 @@
 package world.pasds.back.dashboard.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import world.pasds.back.dashboard.entity.OrganizationDashboard;
 import world.pasds.back.organization.entity.Organization;
@@ -11,7 +13,8 @@ import java.util.Optional;
 @Repository
 public interface OrganizationDashboardRepository extends JpaRepository<OrganizationDashboard, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<OrganizationDashboard> findByYearAndMonthAndOrganization(int year, int month, Organization organization);
     List<OrganizationDashboard> findByOrganization(Organization organization);
 
-}
+}   

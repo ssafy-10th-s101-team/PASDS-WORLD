dda1d865a5dce16b4f350f54f87f99c0816b4d59
유광우 <godsun7892@naver.com>
2024-05-10 16:04:49 +0900

    [BE] fix: <대시보드 동시성 에러 해결>

--- back/src/main/java/world/pasds/back/dashboard/repository/TeamDashboardRepository.java
+++ back/src/main/java/world/pasds/back/dashboard/repository/TeamDashboardRepository.java
@@ -1,6 +1,9 @@
 package world.pasds.back.dashboard.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import world.pasds.back.dashboard.entity.TeamDashboard;
 import world.pasds.back.team.entity.Team;
@@ -10,6 +13,7 @@ import java.util.Optional;
 @Repository
 public interface TeamDashboardRepository extends JpaRepository<TeamDashboard, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<TeamDashboard> findByYearAndMonthAndTeam(int year, int month, Team team);
 
 }

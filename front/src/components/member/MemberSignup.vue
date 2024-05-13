51831cab089a6a90d9ba315936a7c1837e348791
KimJinyong <wlsdyd4@naver.com>
2024-05-13 15:25:24 +0900

    [FE] feat: h

--- front/src/components/member/MemberSignup.vue
+++ front/src/components/member/MemberSignup.vue
@@ -117,7 +117,7 @@
 
         <div class="flex items-center justify-start">
           <button
-            type="button"
+            type="submit"
             class="text-white bg-samsung-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-samsungone700c rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             회원가입

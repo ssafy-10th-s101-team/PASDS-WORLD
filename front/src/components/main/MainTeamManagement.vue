bd7cd69f11a1cd203a2db96077ddeee0916b0ffc
imdeokey <imdeokey@gmail.com>
2024-05-16 16:48:02 +0900

    [FE] design: <다운로드 앱 버튼 삭제, 팀삭제 및 보안강화하기 버튼처럼 보이게>

--- front/src/components/main/MainTeamManagement.vue
+++ front/src/components/main/MainTeamManagement.vue
@@ -248,9 +248,13 @@
                 d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
               />
             </svg>
-            <div @click="updateKey">보안 강화하기</div>
+            <div @click="updateKey">
+              <a href=""> 보안 강화하기 </a>
+            </div>
           </div>
-          <div class="text-red-500" @click="toggleHidden('deleteTeamModal')">팀삭제</div>
+          <div class="text-red-500" @click="toggleHidden('deleteTeamModal')">
+            <a href=""> 팀삭제 </a>
+          </div>
         </div>
       </div>
     </div>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -23,12 +23,12 @@
                     </router-link>
                   </li>
                   <li>
-                    <a
+                    <!-- <a
                       href="#download-section"
                       class="ml-4 py-2 px-4 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 text-gray-800 dark:text-gray-200 rounded-lg transition duration-300"
                     >
                       Download App
-                    </a>
+                    </a> -->
                   </li>
                 </ul>
               </div>

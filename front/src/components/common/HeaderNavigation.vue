c5575fe5982dd1b339b24dec3ba04e0fa5e9d347
imdeokey <imdeokey@gmail.com>
2024-05-16 14:56:53 +0900

    [FE] fix: <로그인 전 헤더바 수정>

--- front/src/components/common/HeaderNavigation.vue
+++ front/src/components/common/HeaderNavigation.vue
@@ -9,14 +9,14 @@
             >
           </router-link>
           <div class="flex items-center lg:order-2">
-            <div class="mt-6">
+            <div v-if="userStore.nickname" class="mt-6">
               <router-link
                 :to="{ name: 'main' }"
                 class="text-gray-800 dark:text-white hover:bg-samsung-blue hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
                 >메인
               </router-link>
             </div>
-            <div class="mt-6">
+            <div v-if="userStore.nickname" class="mt-6">
               <router-link
                 :to="{ name: 'organization' }"
                 class="text-gray-800 dark:text-white hover:bg-samsung-blue hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
@@ -24,7 +24,6 @@
               </router-link>
             </div>
             <div v-if="userStore.nickname" class="mt-6">
-              
               <router-link :to="{ name: 'myPage' }">
                 <span
                   class="text-gray-800 dark:text-white hover:bg-samsung-blue hover:text-white focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
@@ -425,7 +424,6 @@ const logout = async () => {
     router.push({ name: 'home' })
   } catch (error) {}
 }
-
 const jwtTest = async () => {
   try {
     const response = await localAxios.post(`/member/jwt-test`, null)
